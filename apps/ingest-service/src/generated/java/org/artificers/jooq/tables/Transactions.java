8e7eac9b3fa77b601e5ce062aac30ad421b4b742
Keith <keith.drew.13@gmail.com>
2025-09-04 13:36:17 -0700

    Consolidate transactions jOOQ models (#112)

--- apps/ingest-service/src/generated/java/org/artificers/jooq/tables/Transactions.java
+++ apps/ingest-service/src/generated/java/org/artificers/jooq/tables/Transactions.java
@@ -8,8 +8,8 @@ import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
 import org.jooq.impl.TableImpl;
 
-public class CapitalOneTransactions extends TableImpl<Record> {
-    public static final CapitalOneTransactions CAPITAL_ONE_TRANSACTIONS = new CapitalOneTransactions();
+public class Transactions extends TableImpl<Record> {
+    public static final Transactions TRANSACTIONS = new Transactions();
 
     public final TableField<Record, Long> ID = createField(DSL.name("id"), SQLDataType.BIGINT.identity(true), this, "");
     public final TableField<Record, Long> ACCOUNT_ID = createField(DSL.name("account_id"), SQLDataType.BIGINT.nullable(false), this, "");
@@ -19,13 +19,13 @@ public class CapitalOneTransactions extends TableImpl<Record> {
     public final TableField<Record, String> CURRENCY = createField(DSL.name("currency"), SQLDataType.VARCHAR.nullable(false), this, "");
     public final TableField<Record, String> MERCHANT = createField(DSL.name("merchant"), SQLDataType.VARCHAR, this, "");
     public final TableField<Record, String> CATEGORY = createField(DSL.name("category"), SQLDataType.VARCHAR, this, "");
-    public final TableField<Record, String> TXN_TYPE = createField(DSL.name("txn_type"), SQLDataType.VARCHAR, this, "");
     public final TableField<Record, String> MEMO = createField(DSL.name("memo"), SQLDataType.VARCHAR, this, "");
+    public final TableField<Record, String> TXN_TYPE = createField(DSL.name("txn_type"), SQLDataType.VARCHAR, this, "");
     public final TableField<Record, String> HASH = createField(DSL.name("hash"), SQLDataType.VARCHAR.nullable(false), this, "");
     public final TableField<Record, JSONB> RAW_JSON = createField(DSL.name("raw_json"), SQLDataType.JSONB.nullable(false), this, "");
     public final TableField<Record, OffsetDateTime> CREATED_AT = createField(DSL.name("created_at"), SQLDataType.TIMESTAMPWITHTIMEZONE, this, "");
 
-    private CapitalOneTransactions() {
-        super(DSL.name("capital_one_transactions"));
+    private Transactions() {
+        super(DSL.name("transactions"));
     }
 }
