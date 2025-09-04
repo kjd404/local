60259486b22cd068831c29cd35ad77c392ae73b4
Keith <keith.drew.13@gmail.com>
2025-09-03 17:17:02 -0700

    Add transactions view unioning institution tables

---
+++ ops/sql/V9__transactions_view.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE VIEW transactions_view AS
+SELECT
+    id,
+    occurred_at,
+    posted_at,
+    amount_cents,
+    currency,
+    merchant,
+    category,
+    txn_type,
+    memo,
+    hash,
+    raw_json,
+    created_at,
+    account_id,
+    'chase'::TEXT AS institution
+FROM chase_transactions
+UNION ALL
+SELECT
+    id,
+    occurred_at,
+    posted_at,
+    amount_cents,
+    currency,
+    merchant,
+    category,
+    txn_type,
+    memo,
+    hash,
+    raw_json,
+    created_at,
+    account_id,
+    'capital_one'::TEXT AS institution
+FROM capital_one_transactions;
