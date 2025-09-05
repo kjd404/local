69d870eab945af39d3c6ea7072f10b8c72bd46b9
Keith <keith.drew.13@gmail.com>
2025-09-05 08:46:08 -0700

    Add ResourcePatternResolver bean (#122)

--- apps/ingest-service/src/main/java/org/artificers/ingest/CsvMappingLoaderConfiguration.java
+++ apps/ingest-service/src/main/java/org/artificers/ingest/CsvMappingLoaderConfiguration.java
@@ -3,12 +3,20 @@ package org.artificers.ingest;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 /**
  * Registers {@link CsvMappingLoader} with constructor-based dependency injection.
  */
 @Configuration
 class CsvMappingLoaderConfiguration {
+    @Bean
+    ResourcePatternResolver resourcePatternResolver(ResourceLoader loader) {
+        return new PathMatchingResourcePatternResolver(loader);
+    }
+
     @Bean
     static BeanDefinitionRegistryPostProcessor csvMappingLoader(MappingFileLocator locator) {
         return new CsvMappingLoader(locator);
